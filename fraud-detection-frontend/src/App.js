6873dd5a42cd94138d06aa3d1343cd267046ea9d
AhmedTShah <150622814+AhmedTShah@users.noreply.github.com>
2025-05-03 21:41:22 +0500

    Update App.js

--- fraud-detection-frontend/src/App.js
+++ fraud-detection-frontend/src/App.js
@@ -1,36 +1,9 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+
 
 import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
@@ -38,26 +11,36 @@ import Transactions from "./pages/Transactions";
 import FraudLogs from "./pages/FraudLogs";
 import AddFraudRule from "./pages/AddFraudRule"
 import AccountsPage from "./pages/AccountsPage"
-// Additional page like Accounts or FraudRules can be added similarly
+
+
 
 const App = () => {
   return (
     <Router>
       <Box sx={{ display: "flex" }}>
         <Sidebar />
-        <Box component="main" sx={{ flexGrow: 1, p: 3, marginLeft: "240px" }}>
-          <Navbar />
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/transactions" element={<Transactions />} />
-            <Route path="/fraud-logs" element={<FraudLogs />} />
-            <Route path="/fraud-rules" element={<AddFraudRule />} />
-            <Route path="/accounts" element={<AccountsPage/>} />
-
-            <Route path="/" element={<Navigate to="/dashboard" />} />
-          </Routes>
-        </Box>
+        <Box
+  component="main"
+  sx={{
+    flexGrow: 1,
+    p: 3,
+    marginLeft: { xs: 0, sm: "240px" }, // xs = mobile, sm+ = desktop
+    marginTop: "64px"
+  }}
+>
+<Box sx={{ mt: "64px", ml: { md: "250px" }, p: 2 }}>
+  <Navbar />
+  <Routes>
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/users" element={<Users />} />
+    <Route path="/transactions" element={<Transactions />} />
+    <Route path="/fraud-logs" element={<FraudLogs />} />
+    <Route path="/fraud-rules" element={<AddFraudRule />} />
+    <Route path="/accounts" element={<AccountsPage />} />
+    <Route path="/" element={<Navigate to="/dashboard" />} />
+  </Routes>
+</Box>
+</Box>
       </Box>
     </Router>
   );
